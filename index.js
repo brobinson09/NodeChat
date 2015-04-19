ff63cc10932ba89a8c6cdec4c310c53a23de3ede
Barry <brobinson09@gmail.com>
2015-04-18 20:45:18 -0400

    Port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
-}).listen(5000);
-console.log('Server is running at localhost:3000');
+}).listen(port);
+console.log('Server is running at localhost:' +port);
