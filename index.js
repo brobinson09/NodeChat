e1b6810f14b591b736b6fb5c7177ea3167bb75a3
Barry <brobinson09@gmail.com>
2015-04-18 21:35:05 -0400

    display a random windows desktop image

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var http = require('http');
+var fs = require('fs');
+
 port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.writeHead(200,{'Content-Type': 'image/jpg'});
+	fs.readdir('./images', function(err, files) {
+		console.log(files);
+		if(err) {
+			throw err;
+		}
+		else{
+			image = Math.floor(Math.random() * (files.length) );
+			fs.createReadStream('./images/'+files[image]).pipe(res);
+		}
+	});
 }).listen(port);
 console.log('Server is running at localhost:' +port);
